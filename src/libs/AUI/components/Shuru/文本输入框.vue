b62ad5d77b617fa5115f9e1f2b3866ff7bdf619a
Sdator <250740270@qq.com>
2021-08-05 07:57:13 +0800

    完善输入框显示格式

--- src/libs/AUI/components/Shuru/文本输入框.vue
+++ src/libs/AUI/components/Shuru/文本输入框.vue
@@ -2,20 +2,24 @@
   <div>
     <textarea style="" v-model="blob"></textarea>
 
-    <span>字符统计:{{ num }}</span>
+    <span>字符统计:{{ num.toString().padStart(3, "0") }}</span>
   </div>
 </template>
 
 
 <script setup>
 import { computed, reactive, toRefs, warn, watch } from "@vue/runtime-core";
+import { debounce } from "../../../api.js";
 
 const data = reactive({
   num: computed(() => {
     console.log("文本发生变化");
-    return data.blob.length;
+
+    // 去掉空格后的数量
+    return data.blob.replace(/ /g, "").length;
   }),
   blob: "",
+  debounce: debounce(500),
 });
 
 const 延时 = (time) => {
@@ -32,32 +36,32 @@ const 延时 = (time) => {
   });
 };
 
-const 防抖 = (fun, time = 1000) => {
-  let t;
-  console.log(t, "执行了");
-  return function () {
-    console.log(t, 22222);
-    if (t) {
-      return;
-    }
-    t = setTimeout(() => {
-      console.log(t, 33333333333);
-
-      fun();
-      t = null;
-    }, time);
-  };
-};
-
+const regex = new RegExp(/[a-fA-F0-9]/g);
 watch(
   () => data.blob,
-  async (v) => {
-    防抖(() => console.log(1111))();
-
-    // await 延时(1000).then(() => {
-    //   //   去掉前后空字符 有禁止回车的效果
-    //   data.blob = v.trim();
-    // });
+  (v, old) => {
+    // 没有手感 适合任意字符输入时使用  空格换行除外
+    // if (!regex.test(v[v.length - 1])) data.blob = old;
+    // data.blob = data.blob.trim().toUpperCase();
+    // 防抖函数
+    data.debounce(() => {
+      const arr = [];
+      // 匹配正规的字符 并每两个字符合并为一个数组添加到 arr
+      const blob = v.match(regex)?.forEach((_, k, c) => {
+        //条件 双数触发一次
+        if (k % 2 == 0) {
+          arr.push(
+            //添加到数组中
+            c
+              .slice(k, k + 2) //取两个元素
+              .join("") //合并
+              .toUpperCase() //转换为大小写
+          );
+        }
+      });
+      // 最后把数组转为字符串并用空格分隔
+      data.blob = arr.join(" ");
+    });
   }
 );
 
@@ -75,10 +79,12 @@ div {
   position: relative;
 
   textarea {
+    // 禁止窗口缩放
     resize: none;
     height: 100%;
     width: 100%;
-    overflow-y: scroll;
+    // 设置y轴滚动条
+    // overflow-y: scroll;
   }
 
   span {
