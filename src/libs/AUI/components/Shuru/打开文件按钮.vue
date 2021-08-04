5590eab233cdcd2178677e188f27733f4b27d05e
Sdator <250740270@qq.com>
2021-08-05 02:38:54 +0800

    新增功能按钮组件

---
+++ src/libs/AUI/components/Shuru/打开文件按钮.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 代理触发 -->
+  <input
+    ref="openfileEl"
+    v-show="false"
+    type="file"
+    @change="(e) => emits('inFile', e.target.files)"
+  />
+  <button
+    class="mdui-btn mdui-color-theme-accent mdui-ripple"
+    @click="openfileEl.click()"
+  >
+    {{ title }}
+  </button>
+</template>
+
+<script setup>
+/**
+ * 此组件用于点击按钮返回打开的文件列表
+ */
+import { ref, defineEmits } from "vue";
+const openfileEl = ref(null);
+const props = defineProps(["title"]);
+const emits = defineEmits(["inFile"]);
+</script>
+
+<style scoped>
+button {
+  width: 200px;
+  height: 60px;
+  margin: 10px;
+}
+</style>
