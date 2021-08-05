78da1169db5b3ebdea3bccbb3216caa3c77df33f
Sdator <250740270@qq.com>
2021-08-05 18:59:21 +0800

    使用mdui插件

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from "./App.vue";
 
 // 安装插件
 import AUI from "./libs/AUI/index.js";
+import MDUI from "mdui";
 
 // 使用插件 use(AUI)
-const vm = createApp(App).use(AUI).mount("#app");
+const vm = createApp(App).use(AUI).use(MDUI).mount("#app");
 
 console.log(vm, 1111111);
